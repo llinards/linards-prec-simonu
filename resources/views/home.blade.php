6c1dd67ed24336c8a743cdeb64ca1d9f54fc6748
Linards <linards_lazdins@hotmail.com>
2023-12-18 22:03:55 +0200

    Update section height

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <section
-        class="introduction w-screen h-screen flex flex-col justify-center items-center bg-cover bg-center relative"
-        style="background-image: url('{{ asset('storage/intro-background.jpg')}}')">
+        class="introduction w-screen flex flex-col justify-center items-center bg-cover bg-center relative"
+        style="background-image: url('{{ asset('/storage/intro-background.jpg')}}')">
         <div class="flex flex-col justify-center items-center h-full">
             <h1 class="lg:text-9xl md:text-6xl text-4xl font-bold text-white backdrop-blur-sm">Simona & Linards</h1>
         </div>
