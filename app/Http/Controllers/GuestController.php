5e0b7996c0bb0ba536b7fa345a710799df6aa5d7
Linards <linards_lazdins@hotmail.com>
2023-12-20 22:00:23 +0200

    Add guest to the database

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreGuestRequest;
+use App\Models\Guest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
@@ -11,10 +12,11 @@ class GuestController extends Controller
     public function store(StoreGuestRequest $data)
     {
         try {
-            return redirect()->to(app('url')->previous()."#rsvp")->with('message', 'Paldies! Informācija reģistrēta!');
+            Guest::create($data->validated());
+            return redirect()->to(app('url')->previous()."#rsvp")->with('success', 'Paldies! Informācija reģistrēta!');
         } catch (\Throwable $th) {
             Log::error($th);
-            return redirect()->to(app('url')->previous()."#rsvp")->with('message', 'Notikusi kļūda!');
+            return redirect()->to(app('url')->previous()."#rsvp")->with('error', 'Notikusi kļūda! Mēģini vēlreiz.');
         }
     }
 }
--- resources/views/components/status-message.blade.php
+++ resources/views/components/status-message.blade.php
@@ -1,4 +1,13 @@
-<div id="status-message" class="p-4 my-4 text-sm rounded-lg text-white text-center font-bold"
-     role="alert">
-    {{$slot}}
-</div>
+@if(session()->has('success'))
+    <div id="success-message" class="p-4 my-4 text-sm rounded-lg text-white text-center font-bold"
+         role="alert">
+        {{Session::get('success')}}
+    </div>
+
+@elseif(session()->has('error'))
+    <div id="error-message" class="p-4 my-4 text-sm rounded-lg text-white text-center font-bold"
+         role="alert">
+        {{Session::get('error')}}
+    </div>
+@endif
+
