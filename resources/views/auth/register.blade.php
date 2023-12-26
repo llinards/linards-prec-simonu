4658643a0ab6a756a5e085159d888ae8b4a3c731
Linards <linards_lazdins@hotmail.com>
2023-12-26 12:07:07 +0200

    Add translations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -42,11 +42,6 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-               href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
             <x-primary-button type="submit"
                               class="bg-gray-800 hover:bg-gray-300 focus:bg-gray-300 text-white hover:text-black focus:text-black ms-3">
                 {{ __('Register') }}
