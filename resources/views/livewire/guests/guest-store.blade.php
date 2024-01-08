204eb678e6969f260c185e0819b679519bdb55ac
Linards <linards_lazdins@hotmail.com>
2024-01-08 22:10:03 +0200

    Update button style

--- resources/views/livewire/guests/guest-store.blade.php
+++ resources/views/livewire/guests/guest-store.blade.php
@@ -56,7 +56,7 @@
                 <x-input-error :messages="$errors->get('is_staying')" class="mt-2 text-white"/>
             </fieldset>
             <div class="flex justify-center">
-                <x-primary-button class="bg-white text-black" type="submit">
+                <x-primary-button class="bg-white hover:bg-gray-300 focus:bg-gray-300" type="submit">
                     Apstiprinu
                 </x-primary-button>
             </div>
@@ -64,7 +64,8 @@
     @endif
     @if($after_submit)
         <div class="flex justify-center max-w-md mx-auto sm:px-0 px-5">
-            <x-primary-button class="bg-white text-black" type="button" wire:click="toggleFormVisibility">
+            <x-primary-button class="bg-white hover:bg-gray-300 focus:bg-gray-300" type="button"
+                              wire:click="toggleFormVisibility">
                 Pievienot vēl viesi
             </x-primary-button>
         </div>
