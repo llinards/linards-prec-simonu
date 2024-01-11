d2b489c4a9184476b3f2276f4b0701a3cfb859c4
Linards <linards_lazdins@hotmail.com>
2024-01-11 16:02:08 +0200

    Add option to upload images for gallery from admin page

--- resources/views/gallery/index.blade.php
+++ resources/views/gallery/index.blade.php
@@ -3,16 +3,21 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <h2>{{ __('Add images') }}</h2>
-                <x-status-message/>
-                <form method="POST" action="{{route('gallery.store')}}" enctype="multipart/form-data">
+                <h2 class="text-2xl mb-3">{{ __('Add images') }}</h2>
+                <x-admin-status-message/>
+                <x-input-error :messages="$errors->get('gallery-images')" class="my-3"/>
+                <form method="POST" action="{{route('gallery.store')}}" enctype="multipart/form-data"
+                      class="mb-3">
                     @csrf
-                    <x-file-upload :name="'gallery'"/>
+                    <x-file-upload :name="'gallery-images'"/>
                     <x-primary-button :type="'submit'"
-                                      class="bg-gray-800 hover:bg-gray-300 focus:bg-gray-300 text-white hover:text-black focus:text-black ms-3">
+                                      class="mt-3 bg-gray-800 hover:bg-gray-300 focus:bg-gray-300 text-white hover:text-black focus:text-black">
                         {{ __('Add') }}
                     </x-primary-button>
                 </form>
+                <p class="text-sm">Bildei ir jābūt .JPG, .JPEG vai .PNG formātā un pēc iespējas mazākā izmērā.</p>
+                <p class="text-sm">Tās var samazināt šajā lapā - <a href="https://compressor.io/" class="underline"
+                                                                    target="_blank">compressor.io</a>
             </div>
         </div>
     </div>
