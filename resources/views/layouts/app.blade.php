eb6830f43cc6e8c8573ef1dfa89301e1b616f80b
Linards <9407839+llinards@users.noreply.github.com>
2024-01-16 20:40:08 +0200

    Remove unnecessary div

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 @include('layouts.partials.header')
 <body class="font-sans antialiased">
-<div class="bg-gray-100">
-    <main class="h-dvh overflow-y-scroll snap-mandatory snap-y scroll-smooth">
-        {{ $slot }}
-    </main>
-</div>
+<main class="h-dvh overflow-y-scroll snap-mandatory snap-y scroll-smooth">
+    {{ $slot }}
+</main>
 </body>
 </html>
