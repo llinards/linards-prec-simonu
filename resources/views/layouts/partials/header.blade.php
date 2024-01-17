6c1fae2e1f04833a4bc568bf7b029f7aac64a6b3
Linards <9407839+llinards@users.noreply.github.com>
2024-01-17 12:36:18 +0200

    Update meta tags for social media

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -9,6 +9,21 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
+    <!-- Facebook Meta Tags -->
+    <meta property="og:url" content="{{Request::url()}}">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="Linards prec Simonu">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="{{asset('storage/intro-background.jpg')}}">
+
+    <!-- Twitter Meta Tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="twitter:domain" content="{{Request::url()}}">
+    <meta property="twitter:url" content="{{Request::url()}}">
+    <meta name="twitter:title" content="Linards prec Simonu">
+    <meta name="twitter:description" content="">
+    <meta name="twitter:image" content="{{asset('storage/intro-background.jpg')}}">
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
