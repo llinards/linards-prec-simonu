855aae728c105fe88589682ef1b2dacd1cbb8e68
Linards <9407839+llinards@users.noreply.github.com>
2024-01-18 11:27:53 +0200

    Update header.blade.php

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -16,7 +16,11 @@
 
         gtag('config', 'G-KLCJQL9ZE7');
     </script>
-    
+
+    <script id="mcjs">!function (c, h, i, m, p) {
+            m = c.createElement(h), p = c.getElementsByTagName(h)[0], m.async = 1, m.src = i, p.parentNode.insertBefore(m, p)
+        }(document, "script", "https://chimpstatic.com/mcjs-connected/js/users/97b0dbc536f2d99466f817829/793658c59ed69a70b40aa2937.js");</script>
+
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
